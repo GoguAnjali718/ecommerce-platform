fe95f3f045af534a23a4310b3628f7a1ba7bc7a8
Anjali Gogu <anjali.gogu@everest.engineering>
2024-12-12 11:05:41 +0530

    Add App component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  it("renders the App component", () => {
+    render(<App />);
+    const loginPageElement = screen.getByTestId("loginComponent");
+    expect(loginPageElement).toBeInTheDocument();
+  });
+  it("should render the login page correctly", () => {
+    render(<App />);
+    const loginElement = screen.getByTestId("loginComponent");
+    expect(loginElement).toBeInTheDocument();
+  });
 });
