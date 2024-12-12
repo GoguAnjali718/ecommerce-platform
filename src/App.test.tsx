7eab61a246f38db5916d59136a791d991cf20b7d
Anjali Gogu <anjali.gogu@everest.engineering>
2024-12-12 22:03:26 +0530

    update the App test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,5 @@ import App from "./App";
 describe("App Component", () => {
   it("renders the App component", () => {
     render(<App />);
-    const loginPageElement = screen.getByTestId("loginComponent");
-    expect(loginPageElement).toBeInTheDocument();
-  });
-  it("should render the login page correctly", () => {
-    render(<App />);
-    const loginElement = screen.getByTestId("loginComponent");
-    expect(loginElement).toBeInTheDocument();
   });
 });
