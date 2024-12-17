10bf603e3049bf0374c864c4b5b9d526dd649721
Anjali Gogu <anjali.gogu@everest.engineering>
2024-12-18 02:06:43 +0530

    add the unit test for product page

--- src/components/products/ProductPage.test.tsx
+++ src/components/products/ProductPage.test.tsx
@@ -1,2 +1,26 @@
 import React from "react";
 import { Screen, fireEvent, render } from "@testing-library/react";
+import { ListOfProducts } from "./ProductPage";
+import { ProductProvider } from "../../contexts/ProductContext";
+import { useProductContext } from "../../contexts/ProductContext";
+
+jest.mock("../../contexts/ProductContext");
+
+describe("ProductPage", () => {
+  const mockProductsData = {
+    productData: [
+      {
+        productName: "Kurta",
+        price: "1200",
+        rating: "4",
+        sizes: ["S", "M", "L", "XL"],
+        imageUrl:
+          "/Users/anjaligogu/Documents/CODE/myntra/src/components/assets/imageUrls-7.jpeg",
+        reviews: "The product is very comfortable and fits perfectly!",
+      },
+    ],
+  };
+  beforeEach(() => {
+    (useProductContext as jest.Mock).mockReturnValue(mockProductsData);
+  });
+});
